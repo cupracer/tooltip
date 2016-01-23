ada34fd27550311e12925b5e6948fd5319325cb5
Thomas Schulte <thomas@cupracer.de>
2016-01-23 02:00:03 +0100

    update copyright year

--- lib/thumbsniper/common/TooltipSettings.php
+++ lib/thumbsniper/common/TooltipSettings.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2015  Thomas Schulte <thomas@cupracer.de>
+ * Copyright (C) 2016  Thomas Schulte <thomas@cupracer.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
